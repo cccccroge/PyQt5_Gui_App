f830b72c92f531fc41f0d5d1855ea6322916446d
cccccroge <pooopooo543@gmail.com>
2018-08-03 14:48:23 +0800

    Set sizes of splitter and try to add slot to close tab

--- central.py
+++ central.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
+
 
 class central(QtWidgets.QWidget):
     def __init__(self, parent, **kwargs):
@@ -10,14 +11,24 @@ class central(QtWidgets.QWidget):
         centralWidget.setTabsClosable(True)
         centralWidget.setMovable(True)
         centralWidget.setTabShape(QtWidgets.QTabWidget.Triangular)
+        centralWidget.tabCloseRequested.connect(lambda: self.on_centralWidget_tabCloseRequested())
+        self.connect(centralWidget, QtCore.pyqtSignal('tabCloseRequested(int)'),
+            self, QtCore.pyqtSlot(on_centralWidget_tabCloseRequested))
         parent.setCentralWidget(centralWidget)
 
         # Main work space (loaded default)
-        parent.mainWidget = QtWidgets.QSplitter()
+        parent.mainWidget = QtWidgets.QSplitter()   # make field of mainWindow to access mainWidget in other places
+        
         propertyWidget = QtWidgets.QWidget()
-        fieldWidget = QtWidgets.QWidget()
+        propertyWidget.sizePolicy().setHorizontalPolicy(QtWidgets.QSizePolicy.Minimum)
         parent.mainWidget.addWidget(propertyWidget)
+
+        fieldWidget = QtWidgets.QWidget()
+        fieldWidget.sizePolicy().setHorizontalPolicy(QtWidgets.QSizePolicy.Expanding)
         parent.mainWidget.addWidget(fieldWidget)
+
+        parent.mainWidget.setStretchFactor(0, 1)
+        parent.mainWidget.setStretchFactor(1, 5)
 
         centralWidget.addTab(parent.mainWidget, self.tr("主要工作面板"))
 
@@ -25,4 +36,7 @@ class central(QtWidgets.QWidget):
         # Test
         w = QtWidgets.QWidget()
         centralWidget.addTab(w, "2")
+
+    def on_centralWidget_tabCloseRequested(self, index):
+        print("YO")
 

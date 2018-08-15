86c7889bc2c22c73f45e51da2eaa0659f7f229fb
cccccroge <pooopooo543@gmail.com>
2018-08-15 16:48:20 +0800

    Add two mapValue parts for two dataTypes

--- blocks/condDataBlock.py
+++ blocks/condDataBlock.py
@@ -15,6 +15,7 @@ class condDataBlock(block.block):
         self.settingDialog.rejected.connect(self.on_settingDialog_rejected)
 
         # Dialog conetent
+        # data type
         groupBoxDatatype = QtWidgets.QGroupBox(self.tr("類型"))
         groupBoxDatatypeLayout = QtWidgets.QVBoxLayout()
         groupBoxDatatype.setLayout(groupBoxDatatypeLayout)
@@ -29,7 +30,41 @@ class condDataBlock(block.block):
 
         groupBoxDatatypeLayout.addWidget(radioBtnExist)
         groupBoxDatatypeLayout.addWidget(radioBtnVal)
-        
+
+        # map values for existence
+        self.groupBoxMaprules = QtWidgets.QGroupBox(self.tr("對應值"))
+        groupBoxMaprulesLayout = QtWidgets.QGridLayout()
+        self.groupBoxMaprules.setLayout(groupBoxMaprulesLayout)
+        #self.settingLayout.addWidget(groupBoxMaprules)
+
+        yesLabel = QtWidgets.QLabel(self.tr("是"))
+        mapIcon1 = QtWidgets.QLabel(self.tr("=>"))
+        self.lineEditYes = QtWidgets.QLineEdit()
+        noLabel = QtWidgets.QLabel(self.tr("否"))
+        mapIcon2 = QtWidgets.QLabel(self.tr("=>"))
+        self.lineEditNo = QtWidgets.QLineEdit()
+
+        groupBoxMaprulesLayout.addWidget(yesLabel, 0, 0)
+        groupBoxMaprulesLayout.addWidget(mapIcon1, 0, 1)
+        groupBoxMaprulesLayout.addWidget(self.lineEditYes, 0, 2)
+        groupBoxMaprulesLayout.addWidget(noLabel, 1, 0)
+        groupBoxMaprulesLayout.addWidget(mapIcon2, 1, 1)
+        groupBoxMaprulesLayout.addWidget(self.lineEditNo, 1, 2)
+
+        # map values for value
+        self.groupBoxMaprules2 = QtWidgets.QGroupBox(self.tr("對應值"))
+        groupBoxMaprulesLayout2 = QtWidgets.QGridLayout()
+        self.groupBoxMaprules2.setLayout(groupBoxMaprulesLayout2)
+        #self.settingLayout.addWidget(groupBoxMaprules2)
+
+        for i in range(5):
+            lineEditfrom = QtWidgets.QLineEdit()
+            mapIcon = QtWidgets.QLabel(self.tr("=>"))
+            lineEditTo = QtWidgets.QLineEdit()
+
+            groupBoxMaprulesLayout2.addWidget(lineEditfrom, i, 0)
+            groupBoxMaprulesLayout2.addWidget(mapIcon, i, 1)
+            groupBoxMaprulesLayout2.addWidget(lineEditTo, i, 2)
 
 
     ####################
